f25a079d6d3e189fce40c8f0b031474f9d407c13
LloydBatty <lloydbatty@trentu.ca>
2024-11-30 17:06:40 -0500

    8th: TASK 2-4 addMember and addBook now include ID check before adding new item to Library

--- Library.java
+++ Library.java
@@ -13,17 +13,37 @@ public class Library {
     private List<Member> members = new ArrayList<Member>();
     private List<Book> books = new ArrayList<Book>();
 
-    // Add a new member to the library
-    public void addMember(Member member) {
-        members.add(member);
+    
+    // Add a new MEMBER to the library
+    public boolean addMember(Member member) {
+    	//check member ID
+    	if (findMemberById(member.getId()) == null) {
+    		//no other member has that ID
+    		members.add(member);
+    		return true; //member added
+    	}
+    	else {
+    		System.out.println("Sorry, a member with that ID already exists");
+    		return false; //no member added
+    	}
     }
     
-    // Add a new book to the library
-    public void addBook(Book book) {
-        books.add(book);
+    
+    // Add a new BOOK to the library
+    public boolean addBook(Book book) {
+    	//check book ID
+    	if (findBookById(book.getId()) == null) {
+    		//no other book has that ID
+    		books.add(book);
+    		return true; //book added
+    	} else {
+    		System.out.println("Sorry, a book with that ID already exists");
+    		return false; //no book added
+    	}
     }
-
-    // Find a member by ID
+    
+    
+    // Find a MEMBER by ID
     public Member findMemberById(int id) {
         for (Member member : members) {
             if (member.getId() == id) {
@@ -33,7 +53,7 @@ public class Library {
         return null;
     }
 
-    // Find a book by ID
+    // Find a BOOK by ID
     public Book findBookById(int id) {
         for (Book book : books) {
             if (book.getId() == id) {
@@ -43,12 +63,12 @@ public class Library {
         return null;
     }
 
-    // Get the list of members
+    // Get the list of MEMBERS
     public List<Member> getMembers() {
         return members;
     }
     
-    // Get the list of books
+    // Get the list of BOOKS
     public List<Book> getBooks() {
         return books;
     }
