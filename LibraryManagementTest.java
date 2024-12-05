7cd971782f354dced1174a281a66c8d9e1475ed5
LloydBatty <lloydbatty@trentu.ca>
2024-12-04 21:18:07 -0500

    17th: TASK 3-2 testBorrowReturn done, and Transaction class returnBook() now boolean from void

--- LibraryManagementTest.java
+++ LibraryManagementTest.java
@@ -8,8 +8,6 @@
 //						I had to use JUnit 5, as my JUnit 4 will
 //						not work and never runs.
 //
-//						Does not work as intended, but I tried 
-//						TASK 3-2 really does not work
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,7 +21,6 @@ class LibraryManagementTest {
 	//our testing library?
 	private Library library = new Library();
 	
-	
 	@Before
 	public void setUp() {
 		//Does not run if I set up here
@@ -34,20 +31,27 @@ class LibraryManagementTest {
 	@Test
 	public void testBookId() throws Exception {
 		try {
+			//Testing Boundary Cases for Books
 			Book Book100 = new Book(100, "Book100");
 			assertTrue(library.addBook(Book100));
+			//Asserted Booked created successfully
 			
 			Book Book999 = new Book(999, "Book999");
 			assertTrue(library.addBook(Book999));			
+			//Asserted Booked created successfully			
 			
 			Book Book1000 = new Book(1000, "Book1000");
 			assertFalse(library.addBook(Book1000));
+			//Asserted Booked DID NOT CREATE
 			
+			//Testing Outside Boundary Cases for Books
 			Book Book77 = new Book(77, "Book77");
 			assertFalse(library.addBook(Book77));
+			//Asserted Booked DID NOT CREATE			
 			
 			Book Book2000 = new Book(2000, "Book2000");
 			assertFalse(library.addBook(Book2000));
+			//Asserted Booked DID NOT CREATE
 		}
 		catch (Exception e) {
 			System.out.println("Exception throw and caught");
@@ -59,12 +63,31 @@ class LibraryManagementTest {
 	@Test
 	public void testBorrowReturn() throws Exception {
 		try {
+			//instantiating test Book and Member
 			Book TestBook = new Book(555, "TestBook");
 			Member TestMember = new Member(5, "TestMember");
 			assertTrue(TestBook.isAvailable()); 
+			//Asserted Book is Available
 			
-		//TODO: do stuff with the single transaction instance I need to get
+			Transaction TransactionI = Transaction.getTransaction();	
+			assertTrue(TransactionI.borrowBook(TestBook, TestMember));
+			//Asserted Book Borrow transaction succeeded 
 			
+			assertFalse(TestBook.isAvailable());
+			//Asserted Book is UNavailable
+			
+			assertFalse(TransactionI.borrowBook(TestBook, TestMember));
+			//Asserted 2nd Book Borrow transaction is UNsucessful
+			
+			assertTrue(TransactionI.returnBook(TestBook, TestMember));
+			//Asserted Book Return transaction succeeded 
+
+			assertTrue(TestBook.isAvailable());
+			//Asserted Book is available from returning
+			
+			assertFalse(TransactionI.returnBook(TestBook, TestMember));
+			//Asserted 2nd Book Return is UNsucessful
+
 		}
 		catch (Exception e) {
 			System.out.println("Exception throw and caught");
--- Transaction.java
+++ Transaction.java
@@ -50,7 +50,7 @@ public class Transaction {
     }
 
     // Perform the returning of a book
-    public void returnBook(Book book, Member member) {
+    public boolean returnBook(Book book, Member member) {
         if (member.getBorrowedBooks().contains(book)) {
             member.returnBook(book);
             book.returnBook();
@@ -58,8 +58,10 @@ public class Transaction {
             System.out.println(transactionDetails);
             //send details to be logged
             saveTransaction(transactionDetails);
+            return true;
         } else {
             System.out.println("This book was not borrowed by the member.");
+            return false;
         }
     }
 
